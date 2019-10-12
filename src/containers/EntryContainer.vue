d486bd73633f76520cb907fe4fb46746441e3a9b
mjha17 <36537048+mjha17@users.noreply.github.com>
2019-10-12 13:58:41 +0200

    Setup App router

---
+++ src/containers/EntryContainer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <div class="flex">
+            <button>Find Quiz</button>
+            <button disabled>Create Quiz</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .flex{
+        display: flex;
+        flex-direction: column;
+    }
+</style>
--- src/router.js
+++ src/router.js
@@ -1,23 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+
+// Import containers
+import EntryContainer from './containers/EntryContainer.vue'
 
 Vue.use(Router)
-
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      name: 'entry',
+      path: '/home',
+      component: EntryContainer,
+      children: [
+        { path: 'findQuiz', component: FindQuizContainer }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      name: 'quiz',
+      path: '/quiz/:_quiz_id',
+      component: QuizContainer,
+      children: [
+        { path: '', component: QuestionContainer },
+        { path: 'result', component: ResultContainer }
+      ]
     }
+
   ]
 })
